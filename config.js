13da41d9cf54bf95f40da8bf48153e5c3cb8611a
Khailan Abdala <khailanabdala15@gmail.com>
2026-09-18 20:12:47 -0300

    Update and rename config.example.js to config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Copie este arquivo para config.js e preencha os dados do seu projeto Supabase.
+const SUPABASE_URL = "https://idqejyfrsibravpybetf.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_publishable_Q8vWo8hKgGcqMaMuTWxwdg_YV4pCK-U";
